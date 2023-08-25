ad81adf684aeed4205d0749b0fe9e4e42f578d26
Henry <hnguyhenry@protonmail.com>
2023-08-25 16:56:25 -0700

    Add multi page tests

---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,24 @@
+describe('Various examples', () => {
+  beforeEach(() => {
+    cy.visit('/examples');
+  });
+  it('multi-page testing', () => {
+    cy.getDataTest('nav-why-cypress').click();
+    cy.location('pathname').should('equal', '/');
+
+    cy.getDataTest('nav-overview').click();
+    cy.location('pathname').should('equal', '/overview');
+
+    cy.getDataTest('nav-fundamentals').click();
+    cy.location('pathname').should('equal', '/fundamentals');
+
+    cy.getDataTest('nav-forms').click();
+    cy.location('pathname').should('equal', '/forms');
+
+    cy.getDataTest('nav-components').click();
+    cy.location('pathname').should('equal', '/component');
+
+    cy.getDataTest('nav-best-practices').click();
+    cy.location('pathname').should('equal', '/best-practices');
+  });
+});
