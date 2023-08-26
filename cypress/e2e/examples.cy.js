b8441ac794d4f4649aa0c61088ea8b9f620a07d6
Henry <hnguyhenry@protonmail.com>
2023-08-25 17:06:49 -0700

    Add tests for fixtures and intercepts

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -21,4 +21,14 @@ describe('Various examples', () => {
     cy.getDataTest('nav-best-practices').click();
     cy.location('pathname').should('equal', '/best-practices');
   });
+
+  it.only('intercepts', () => {
+    cy.intercept('POST', 'http://localhost:3000/examples', {
+      // body: {
+      //   message: 'successfully intercepted request',
+      // },
+      fixture: 'example.json'
+    });
+    cy.getDataTest('post-button').click();
+  });
 });
